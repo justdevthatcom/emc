11e9f492e6d63f1d8e3cb4b66f42c6a9c94741bf
justdevthat <justdevthat@gmail.com>
2024-08-18 14:53:12 +0300

    Init project, main class

---
+++ src/main/java/com/justdevthat/emc/EmcApplication.java
@@ -0,0 +1,13 @@
+package com.justdevthat.emc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmcApplication.class, args);
+	}
+
+}
